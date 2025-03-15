71a11581f55022ab9c0541d59283e7f4da7fce37
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-03-15 21:53:16 +0000

    Refactored modal functionality to improve transitions. Add overlay and edit styles. Updated index.html to reflect changes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,35 +1,66 @@
 /**
  * Event listener triggers function when 'Play Now' button is clicked, making the quiz container appear in the centre of the screen
  */
-const playNowButton = document.getElementById("start-quiz");
-const quizContainer = document.getElementById("quiz-container");
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
-playNowButton.addEventListener("click", function() {
+openModalButtons.forEach(button => {
+button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal);
+
+// Styles for quiz modal
+modal.style.width = "80vw";
+modal.style.height = "80vh";
+modal.style.backgroundColor = "#58653d";
+modal.style.color = "white";
+modal.style.position = "fixed";
+modal.style.borderRadius = "5px";
+});
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = button.closest(".modal");
+        closeModal(modal);
+    });
+    });
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+};
 
 // Remove the hidden class to make quiz container visible
-quizContainer.classList.remove("hidden");
-
-// Styles for quiz container
-quizContainer.style.width = "85vw";
-quizContainer.style.height = "85vh";
-quizContainer.style.backgroundColor = "#58653d";
-quizContainer.style.color = "white";
-quizContainer.style.position = "fixed";
-quizContainer.style.borderRadius = "5px";
-});
+//modal.classList.remove("hidden");
 
 /**
  * Function to change style of 'Play Now' button when hovered over
  */
-playNowButton.addEventListener("mouseover", function() {
-playNowButton.style.backgroundColor = "#000000";
-});
+//playNowButton.addEventListener("mouseover", function() {
+//playNowButton.style.backgroundColor = "#000000";
+//});
 
-playNowButton.addEventListener("mouseout", function() {
-playNowButton.style.backgroundColor = "#58653d";
-});
+//playNowButton.addEventListener("mouseout", function() {
+//playNowButton.style.backgroundColor = "#58653d";
+//});
 
-//CloseContainer function
+//CloseContainer function - see how to play section for button
 
 //ScoreFunction
 
