7b67de8d3d25e3b0f913e80c4c8a5a2710304c90
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-03-15 08:54:45 +0000

    Add event listener to how to play button. Add new js file and container in index file

---
+++ assets/js/howtoplay.js
@@ -0,0 +1,16 @@
+/**
+ * Event listener triggers function when 'Play Now' button is clicked, making the quiz container appear in the centre of the screen
+ */
+document.getElementById("howtoplay").addEventListener("click", function() {
+    const howToPlayContainer = document.getElementById("howtoplay-container");
+
+// Remove the hidden class to make quiz container visible
+howToPlayContainer.classList.remove("hidden");
+
+// Styles for quiz container
+howToPlayContainer.style.width = "85vw";
+howToPlayContainer.style.height = "85vh";
+howToPlayContainer.style.backgroundColor = "grey";
+howToPlayContainer.style.color = "white";
+howToPlayContainer.style.position = "fixed";
+});
