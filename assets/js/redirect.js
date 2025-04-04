3531ed2fa544eebf9628ade1d80e1da46d35817f
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-04-04 15:36:13 +0100

    Add 404.html and add script for redirect to homepage

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,4 +1,1 @@
-if (window.location.hostname === "davidrc-projects.github.io" && 
-  window.location.pathname === "/Joint-Effort-/") {
 window.location.replace("https://davidrc-projects.github.io/Joint-Effort/");
-}
