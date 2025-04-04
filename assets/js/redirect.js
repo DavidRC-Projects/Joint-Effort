92e65cd8997ccb695cde5946d34b333577c1a789
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-04-04 15:18:55 +0100

    Refactor remove meta direct and add js file to create a redirect

---
+++ assets/js/redirect.js
@@ -0,0 +1,3 @@
+if (window.location.href.indexOf('https://davidrc-projects.github.io/Joint-Effort-Test') === 0) {
+    window.location.href = 'https://davidrc-projects.github.io/Joint-Effort/';
+  };
